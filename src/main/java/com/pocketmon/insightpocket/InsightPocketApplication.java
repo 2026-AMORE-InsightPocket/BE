c353eebaa60c37901522f90ff6e7adde290e780f
HeejuKo <khjg00gle96@gmail.com>
2025-12-29 20:08:30 +0900

    🎉 Init: 프로젝트 세팅

---
+++ src/main/java/com/pocketmon/insightpocket/InsightPocketApplication.java
@@ -0,0 +1,13 @@
+package com.pocketmon.insightpocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightPocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightPocketApplication.class, args);
+    }
+
+}
