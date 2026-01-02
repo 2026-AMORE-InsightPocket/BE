7115203deda2c88e51a2af0dfc2780c3a2e221f6
HeejuKo <khjg00gle96@gmail.com>
2026-01-02 17:03:41 +0900

    ✨ Feat: 지난 달 매출 1위 조회 API 구현
    Feat: 지난 달 매출 1위 조회 API 구현

--- src/main/java/com/pocketmon/insightpocket/domain/dashboard/controller/BestsellerController.java
+++ src/main/java/com/pocketmon/insightpocket/domain/dashboard/controller/BestsellerController.java
@@ -1,6 +1,8 @@
 package com.pocketmon.insightpocket.domain.dashboard.controller;
 
+import com.pocketmon.insightpocket.domain.dashboard.dto.BestsellerTop1Response;
 import com.pocketmon.insightpocket.domain.dashboard.dto.BestsellerTop5Response;
+import com.pocketmon.insightpocket.domain.dashboard.service.BestsellerTop1Service;
 import com.pocketmon.insightpocket.domain.dashboard.service.BestsellerTop5Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/dashboard")
-public class BestsellerTop5Controller {
+public class BestsellerController {
 
     private final BestsellerTop5Service bestsellerTop5Service;
+    private final BestsellerTop1Service bestsellerTop1Service;
 
     @GetMapping("/bestsellers/top5")
     public BestsellerTop5Response top5(@RequestParam String month) {
         return bestsellerTop5Service.getLaneigeTop5(month);
+    }
+
+    @GetMapping("/bestsellers/top1")
+    public BestsellerTop1Response top1(@RequestParam String month) {
+        return bestsellerTop1Service.getLastMonthWinner(month);
     }
 }
