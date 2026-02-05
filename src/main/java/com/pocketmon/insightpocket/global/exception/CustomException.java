b1b1dd1b37682bb22f22f4fe5902aaacc7f2193c
HeejuKo <khjg00gle96@gmail.com>
2026-02-05 20:48:21 +0900

    ♻️ Refactor: 전역 예외 처리 추가 (#45)

--- src/main/java/com/pocketmon/insightpocket/global/exception/CustomException.java
+++ src/main/java/com/pocketmon/insightpocket/global/exception/CustomException.java
@@ -1,4 +1,20 @@
 package com.pocketmon.insightpocket.global.exception;
 
-public class CustomException {
+import com.pocketmon.insightpocket.global.response.BaseCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final BaseCode errorCode;
+
+    public CustomException(BaseCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public CustomException(BaseCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
 }
