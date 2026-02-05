bb38fce5986d98abab2eb35d1d13d1eaeda2b088
HeejuKo <khjg00gle96@gmail.com>
2026-02-06 05:28:54 +0900

    🐛 Fix: 급상승 제품 쿼리 하드 코딩 수정

--- src/main/java/com/pocketmon/insightpocket/domain/dashboard/controller/DashboardController.java
+++ src/main/java/com/pocketmon/insightpocket/domain/dashboard/controller/DashboardController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @Tag(
         name = "요약 대시보드 조회 API"
 )
