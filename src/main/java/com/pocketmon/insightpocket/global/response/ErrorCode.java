9ff6f208397d2810330adb33e1d6095f3082d79b
HeejuKo <khjg00gle96@gmail.com>
2026-02-06 07:19:02 +0900

    🐛 Fix: 데이터 없으면 null 처리 (예외 x)

--- src/main/java/com/pocketmon/insightpocket/global/response/ErrorCode.java
+++ src/main/java/com/pocketmon/insightpocket/global/response/ErrorCode.java
@@ -8,14 +8,19 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode implements BaseCode {
 
+    // 공통
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON-001", "서버 오류가 발생했습니다."),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "COMMON-002", "요청 파라미터가 올바르지 않습니다."),
     VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "COMMON-003", "입력값 검증에 실패했습니다."),
     JSON_PARSE_ERROR(HttpStatus.BAD_REQUEST, "COMMON-004", "JSON 파싱에 실패했습니다."),
     DATA_INTEGRITY_VIOLATION(HttpStatus.CONFLICT, "COMMON-005", "데이터 무결성 제약조건 위반입니다."),
 
+    // 인증
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH-001", "인증에 실패했습니다."),
     INGEST_API_KEY_NOT_CONFIGURED(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH-002", "서버 인증 설정이 누락되었습니다."),
+
+    // 리뷰 분석
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "REVIEW-001", "해당 상품의 리뷰 데이터가 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
