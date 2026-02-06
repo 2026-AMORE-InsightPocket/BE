109a1fa0a916be19a06933a08e6afc209acba118
HeejuKo <khjg00gle96@gmail.com>
2026-02-06 21:21:57 +0900

    ♻️ Refactor: 적재 API 공통 에러 코드 추가

--- src/main/java/com/pocketmon/insightpocket/global/response/ErrorCode.java
+++ src/main/java/com/pocketmon/insightpocket/global/response/ErrorCode.java
@@ -15,6 +15,14 @@ public enum ErrorCode implements BaseCode {
     JSON_PARSE_ERROR(HttpStatus.BAD_REQUEST, "COMMON-004", "JSON 파싱에 실패했습니다."),
     DATA_INTEGRITY_VIOLATION(HttpStatus.CONFLICT, "COMMON-005", "데이터 무결성 제약조건 위반입니다."),
 
+    // 크롤링 데이터 DB 적재
+    INGEST_BAD_REQUEST(HttpStatus.BAD_REQUEST, "INGEST-400", "크롤링 적재 요청 데이터가 올바르지 않습니다."),
+    INGEST_DUPLICATE_SNAPSHOT(HttpStatus.CONFLICT, "INGEST-409", "이미 동일한 스냅샷 데이터가 존재합니다."),
+    INGEST_DATA_INCONSISTENT(HttpStatus.CONFLICT, "INGEST-409-1", "크롤링 데이터 간 정합성이 맞지 않습니다."),
+    INGEST_TARGET_NOT_FOUND(HttpStatus.NOT_FOUND, "INGEST-404", "적재 대상 리소스를 찾을 수 없습니다."),
+    INGEST_PARTIAL_FAILURE(HttpStatus.MULTI_STATUS, "INGEST-207", "일부 데이터 적재에 실패했습니다."),
+    INGEST_PROCESSING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "INGEST-500", "크롤링 데이터 적재 중 오류가 발생했습니다."),
+
     // 인증
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH-001", "인증에 실패했습니다."),
     INGEST_API_KEY_NOT_CONFIGURED(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH-002", "서버 인증 설정이 누락되었습니다."),
